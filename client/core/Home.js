6efcf81e42436f974a22c8f4a92b724280577c69
Fin1ay <f1n1ay@hotmail.com>
2021-03-11 21:02:17 +0000

    Working locally

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
-import Link from 'react-dom'
+import {Link} from 'react-router-dom'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
@@ -38,8 +38,6 @@ export default function Home(){
  Home Page
  </Typography>
  <CardMedia className={classes.media} image={myImg} title="My Image"/>
- <Link to="/users"> Users </Link>
-
  <Typography variant="body2" component="p" className={classes.credit}
 color="textSecondary">Photo: Picasso</Typography>
  <CardContent>
