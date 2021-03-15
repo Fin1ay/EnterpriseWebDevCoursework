9e05c7bd9feeac10485096e151557a3be4a26eb8
Fin1ay <f1n1ay@hotmail.com>
2021-03-15 22:21:35 +0000

    Given up on grid display after full day of adjustments

--- client/core/Selection.js
+++ client/core/Selection.js
@@ -9,60 +9,77 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import myImg from './../assets/images/200x100.jpg'
 
-// create class
-class ImageGrid extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            images: [
-                {name:'Image 1',src:myImg,desc:'This describes this image..'},
-                {name:'Image 2',src:myImg,desc:'This describes this image 2..'},
-                {name:'Image 3',src:myImg,desc:'This describes this image 3 ..'},
-                {name:'Image 4',src:myImg,desc:'This describes this image 4..'},
-                {name:'Image 5',src:myImg,desc:'This describes this image 5..'},
-                {name:'Image 6',src:myImg,desc:'This describes this image 6..'},
-            ],
-            currentSelection: {},
-        };
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    componentDidMount() {
-        // set first image selected
-        this.setState({ currentSelection: this.state.images[0] });
-    }
-
-    handleClick(val) {
-        //console.log(val)
-        this.setState({ currentSelection: val });
-    }
-
-    render(){
-        var { images, currentSelection } = this.state;
-        return(
-        <div>
-            <div className="row">
-                <div className="col-md">
-                    <div className="row no-gutters">
-                    {images.map((val, k) => {
-                        return (
-                        <div className="col-sm-4" key={k}>
-                            <img src={val.src} className={'img-fluid ' + (val.src===currentSelection.src?'p-1':'')} onClick={() => this.handleClick(val)} />
-                        </div>)
-                        })
-                    }
-                     </div>
-                </div>
-                <div className="col-md">
-                    <h3 className="font-weight-light">{ currentSelection.name }</h3>
-                    <p>{ currentSelection.desc }</p>
-                </div>
-            </div>
-        </div>
-        )
-    }
-};
+//Attempt at Image grid. Unsuccessful after full day of attempted adjustments
+// // create class
+// class ImageGrid extends React.Component {
+//
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             images: [
+//                 {name:'Image 1',src:myImg,desc:'This describes this image..'},
+//                 {name:'Image 2',src:myImg,desc:'This describes this image 2..'},
+//                 {name:'Image 3',src:myImg,desc:'This describes this image 3 ..'},
+//                 {name:'Image 4',src:myImg,desc:'This describes this image 4..'},
+//                 {name:'Image 5',src:myImg,desc:'This describes this image 5..'},
+//                 {name:'Image 6',src:myImg,desc:'This describes this image 6..'},
+//             ],
+//             currentSelection: {},
+//         };
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+//
+//     componentDidMount() {
+//         // set first image selected
+//         this.setState({ currentSelection: this.state.images[0] });
+//     }
+//
+//     handleClick(val) {
+//         //console.log(val)
+//         this.setState({ currentSelection: val });
+//     }
+//
+//     render(){
+//       //gets images in a collection
+//         var { images, currentSelection } = this.state;
+//         return(
+//         <div>
+//             <div className="row">
+//                 <div className="col-md-4">
+//                     <div className="row no-gutters">
+//                     {images.map((val, k) => {
+//                         return (
+//                         <div className="col-md-4" key={k}>
+//                             <img src={val.src} className={'img-fluid ' + (val.src===currentSelection.src?'p-1':'')} onClick={() => this.handleClick(val)} />
+//                         </div>)
+//                         })
+//                     }
+//                      </div>
+//                 </div>
+//                 <div className="col-md-4">
+//                     <h3 className="font-weight-light">{ currentSelection.name }</h3>
+//                     <p>{ currentSelection.desc }</p>
+//                 </div>
+//             </div>
+//         </div>
+//         )
+//      }
+//  };
+// export default function Selection(){
+//  const classes = useStyles()
+//  return (
+// <Grid container spacing = {10}>
+// <Grid>
+// <Card className = {classes.card}>
+//     <div className="container-fluid py-4">
+//         <h4 className="text-center font-weight-light text-light mb-3">React Gallery with Bootstrap</h4>
+//         <ImageGrid />
+//     </div>
+//   </Card>
+//  </Grid>
+//  </Grid>
+// )
 
 
 
@@ -71,14 +88,16 @@ const useStyles = makeStyles(theme => ({
  maxWidth: 2000,
  margin: 'auto',
  marginTop: theme.spacing(5),
- marginBottom: theme.spacing(5)
+ marginBottom: theme.spacing(5),
+ marginLeft: theme.spacing(5),
+ marginRight: theme.spacing(5)
  },
  title: {
  padding:`${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
  color: theme.palette.openTitle
  },
  media: {
- minHeight: 100
+ minHeight: 400
  },
  credit: {
  padding: 10,
@@ -91,14 +110,13 @@ const useStyles = makeStyles(theme => ({
  }
 }))
 
-
 export default function Selection(){
  const classes = useStyles()
  return (
-<Grid container>
+<Grid container spacing = {10}>
 <Grid>
-  <Card>
-    <div className="container-fluid">
+<Card className = {classes.card}>
+    <div className="container-fluid py-4">
         <h4 className="text-center font-weight-light text-light mb-3">React Gallery with Bootstrap</h4>
         <ImageGrid />
     </div>
